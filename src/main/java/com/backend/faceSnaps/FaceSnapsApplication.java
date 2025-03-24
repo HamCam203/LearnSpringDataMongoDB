203934d84451ebefba57e6c5e8068647a244ae14
HamCam203 <camarahamoudia@gmail.com>
2025-03-24 15:17:13 +0100

    Initial commit

---
+++ src/main/java/com/backend/faceSnaps/FaceSnapsApplication.java
@@ -0,0 +1,13 @@
+package com.backend.faceSnaps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaceSnapsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaceSnapsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/faceSnaps/FaceSnapsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.faceSnaps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaceSnapsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
